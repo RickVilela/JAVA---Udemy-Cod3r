8f8d22cf65948c4d23aa58cc43a12ba3b4a815cd
RickVilela <henrique_vilela_f@hotmail.com>
2022-10-14 11:18:54 -0300

    WorkSpace_Java

---
+++ Aulas/src/fundamentos/AreadaCircunferencia.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class AreadaCircunferencia {
+	
+	public static void main(String[] args) {
+		int raio = 3;
+		final double pi = 3.14159;
+		
+		double area = pi * raio * raio ;
+		
+		System.out.println(area);
+	}
+}
